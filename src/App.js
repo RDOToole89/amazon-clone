f94f587eba94daa7efcd091926da8f4707d1a45d
RDOToole89 <roibinotoole@gmail.com>
2020-10-31 17:16:01 +0100

    transfer static html to react jsx, add ProductCard component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import "./App.css";
+import ProductCard from "./components/ProductCard/ProductCard";
+import data from "./data";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          {data.products.map((product) => {
+            return (
+              <ProductCard
+                key={product._id}
+                rating={product.rating}
+                name={product.name}
+                image={product.image}
+                price={product.price}
+              />
+            );
+          })}
+        </div>
+      </main>
+      <footer className="row center">All right reserved.</footer>
     </div>
   );
 }
